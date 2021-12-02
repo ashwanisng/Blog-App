352fe34ba716527089f37c6cdf6979e83e3c2bd0
ashwanisng <ashwanicena5@gmail.com>
2021-12-02 19:15:22 +0530

    bug fixed

--- lib/app/global/firebase/database/user_db.dart
+++ lib/app/global/firebase/database/user_db.dart
@@ -1,5 +1,4 @@
 import 'package:blog_app/app/core/enviroment/env.dart';
-import 'package:blog_app/app/data/model/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +9,7 @@ class UserDbController extends GetxController {
       FirebaseFirestore.instance.collection('Users');
 
   FirebaseAuth auth = FirebaseAuth.instance;
+  var userData = [].obs;
 
   String? name;
 
@@ -37,6 +37,22 @@ class UserDbController extends GetxController {
         colorText: Colors.white,
         isDismissible: true,
       );
+    }
+  }
+
+  fetchUserData() async {
+    try {
+      var snapshot = await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(auth.currentUser!.uid)
+          .collection("users")
+          .get();
+
+      userData.value = snapshot.docs.map((e) => (e.data())).toList();
+
+      update();
+    } catch (e) {
+      // print(e);/
     }
   }
 }
--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: unnecessary_overrides
 
-import 'package:blog_app/app/data/model/user.dart';
 import 'package:blog_app/app/global/firebase/database/user_db.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
@@ -11,41 +9,21 @@ class ProfileController extends GetxController {
   RxString name = ''.obs;
   RxString photoUrl = ''.obs;
 
-  // UserDbController userDbController = Get.find<UserDbController>();
-  List userData = <UserModel>[];
-  FirebaseAuth auth = FirebaseAuth.instance;
+  UserDbController userDbController = Get.find<UserDbController>();
 
-  fetchUserData() async {
-    try {
-      var snapshot = await FirebaseFirestore.instance
-          .collection("Users")
-          .doc(auth.currentUser!.uid)
-          .collection("users")
-          .get();
-
-      userData = snapshot.docs.map((e) => (e.data())).toList();
-
-      print(userData[0]['name']);
-      userName.value = userData[0]['userName'];
-      name.value = userData[0]['name'];
-      photoUrl.value = userData[0]['photoUrl'];
-
-      update();
-    } catch (e) {
-      print(e);
-    }
+  getData() {
+    userName.value = userDbController.userData[0]['userName'];
+    name.value = userDbController.userData[0]['name'];
+    photoUrl.value = userDbController.userData[0]['photoUrl'];
   }
 
   @override
   void onInit() {
-    fetchUserData();
+    userDbController.fetchUserData();
+
     super.onInit();
 
-    // for (var i = 0; i < userDb.userData.length; i++) {
-    //   name = userDb.userData[i]['name'];
-    //   userName = userDb.userData[i]['userName'];
-    //   photoUrl = userDb.userData[i]['photoUrl'];
-    // }
+    getData();
   }
 
   @override
